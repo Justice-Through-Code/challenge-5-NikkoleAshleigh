96fa6c22603996157cb94340883af2c6c498053d
NikkoleAshleigh <nikkole.hardy.7.1.21@gmail.com>
2022-09-28 13:04:28 -0400

    Looped through data for outcomes using for and while loops with conditional logic

--- week_days.py
+++ week_days.py
@@ -6,13 +6,24 @@ def day_of_week():
     # Let's start simple, and build up from there.
     # 1.1 TODO: Write a for loop that prints out each day in the `days` variable above.
 
+    for single_day in days:
+        print(single_day)
+
     # 1.2 TODO: Write another for loop that does the same thing, but this time use the range function
 
+    for single_day in range(len(days)):
+        print(days[single_day])
+# day_of_week()
 
 def favorite_activities():
     # 2.1 TODO: Now, in a for loop, instead of just printing out the day,
     # let's ask the user what their favorite thing to do is on that day of the week.
     # NOTE: Make sure to use an f-string so that the user knows which day they're being asked about.
+
+    fav_activities = []
+
+    for single_day in days:
+        fav_thing = input(f'What is your favorite thing to do on a {single_day}? ')
 
     # We should keep track of the user's favorite things to do so that we can print them out all together.
     # 2.2 TODO: ABOVE your for loop, create a new empty list to hold the user's favorite activities.
@@ -20,30 +31,44 @@ def favorite_activities():
     # 2.3 TODO: Now, back in your for loop, append each of the user's answers into your new list.
     # AFTER your loop, print out the list to check if it got populated correctly.
 
+        fav_activities.append(fav_thing)
+    print(fav_activities)
+
     # 2.4: After the code you've written so far, let's create a new for loop.
     # As an example, let's say the user's favorite thing to do on Mondays is plan their week.
     #  This time, we want the output to be something like this:
     # f'On Mondays, your favorite activity is to plan your week.'
+
+    for i in range(len(days)):
+        print(f'On {days[i]}s, your favorite activity is to {fav_activities[i]}.')
+
+# ----------------
+    # can make a call in for loop on either function because both lists are the SAME length, 
+    # range of the length of the list is what is considered when making that call WITH [i]
+# -------------------
     # We need information from both lists! Let's use the `range` function to loop through the indexes
     # of the items in the lists (this will work because the lists are the same length).
     # Each time through this new loop, use the index number to index into each of your lists for the data
     # you need to print out.
+# favorite_activities()
 
+# '''
+#     Take a look back at the code you just wrote. Look at how much it does!
+#     Often, programmers will be given large tasks, and it's our responsibility to be able to break it down into
+#     smaller pieces. We did the above piece by piece, but think about what the prompt might have been
+#     to get us there.
 
-'''
-    Take a look back at the code you just wrote. Look at how much it does!
-    Often, programmers will be given large tasks, and it's our responsibility to be able to break it down into
-    smaller pieces. We did the above piece by piece, but think about what the prompt might have been
-    to get us there.
+#     Maybe: Write a program that asks the user about their favorite thing to do each day of the week.
+#     Afterward, print out for the user each of their favorite daily activities.
 
-    Maybe: Write a program that asks the user about their favorite thing to do each day of the week.
-    Afterward, print out for the user each of their favorite daily activities.
-
-    Would this larger task have felt doable without breaking it down into steps?
-    Is it clear what needs to be done?
-
-    Try to break down the steps required for this second loop challenge.
-'''
+#     Would this larger task have felt doable without breaking it down into steps?
+#     Is it clear what needs to be done?
+# ----------------
+#     This program was hard enough with these smaller tasks, it definitely would not have
+#       felt doable without breaking it down into steps! It could have been made a bit more clear.
+# --------------
+#     Try to break down the steps required for this second loop challenge.
+# '''
 
 
 def temp_by_day():
@@ -51,6 +76,16 @@ def temp_by_day():
     # is. If the temperature is below 50, tell the user to 'Brr, put on a jacket!'. Or, if the temperature is
     # between 50 and 65, tell the user to 'Cozy, grab a sweater'. Finally, if the temperature is above 65,
     # tell the user to 'Put on some sunscreen!'.
+    
+    for single_day in days:
+        temp_input = int(input(f'What is the temperature on {single_day}? '))
+        if temp_input < 50:
+            print('Brr, put on a jacket!')
+        elif temp_input < 65:
+            print('Cozy, grab a sweater')
+        else:
+            print('Put on some sunscreen!')
+# temp_by_day()
 
 
 def temp_by_day_continuous():
@@ -60,3 +95,9 @@ def temp_by_day_continuous():
 
     # NOTE: remember, if you accidentally create an infinite while loop, it's ok! Go into the command line and
     # hit control + C to stop the program. No harm done to your computer (:
+    temp = int(input('What is the temperature outside? '))
+    while temp <= 65:
+        print('Wear a sweater')
+        temp = int(input('What is the temperature outside? '))
+    print('Spring has sprung!')
+# temp_by_day_continuous()
